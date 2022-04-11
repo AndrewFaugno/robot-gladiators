2d310beafdf8fcb482789436b8a802bd6a1499a8
Andrew Faugno <andrewfaugno825@gmail.com>
2022-04-11 15:13:54 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,7 +65,23 @@ var fight = function(enemyName) {
   }; // end of fight function
 
 for (var i = 0; i < enemyNames.length; i++) {
+  if (playerHealth > 0) {
+
+    // let the player know what round they are in
+    window.alert("Welcome to Robot Gladiators! Round " + ( i  + 1) );
+
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight
     enemyHealth = 50;
-    fight(enemyNames[i]);
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+  }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
+  
 }      
